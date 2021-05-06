06b3ad1ff46ea97e38258cc255f5d2e92a629081
bradygrapentine <brady.grapentine@gmail.com>
2021-05-05 21:16:52 -0400

    having trouble with the CreateAToDoListAsync method, can't get the POST requests to go through

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Net.Http;
+using Newtonsoft.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using ConsoleTables;
-
+using System.Text;
+using System.Text.Unicode;
 
 namespace OneListClient
 {
@@ -44,30 +46,189 @@ namespace OneListClient
     }
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task SeeAllToDoListItemsAsync(string token)
         {
             var client = new HttpClient();
-            // var args = {};
+            var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
+            var responseAsStream = await client.GetStreamAsync(url);
+            var items = await System.Text.Json.JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+            var table = new ConsoleTable("Description", "Created At", "Updated At", "Completed");
+            Console.WriteLine();
+            foreach (var item in items)
+            {
+                table.AddRow(item.Text, item.CreatedAt, item.UpdatedAt, item.CompletedStatus);
+            }
+            Console.WriteLine(table);
+        }
+        static async Task CreateToDoListItemAsync(string token)
+        {
+            var client = new HttpClient();
+            Console.Write("What is the name of your new to-do list item? ");
+            var name = Console.ReadLine();
+            var newItem = new Item();
+            newItem.Text = name;
+            var updateInput = JsonConvert.SerializeObject(newItem);
+            // var buffer = System.Text.Encoding.UTF8.GetBytes(updateInput);
+            // var byteContent = new ByteArrayContent(buffer);
+            // byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var updateContent = new StringContent(updateInput, Encoding.UTF8, "application/json");
+            var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
+            // var responseAsStream = await client.PostAsync(url, updateContent).Result; This line throws an error
+            var responseAsStream = client.PostAsync(url, updateContent).Result;
+            // var result = await client.PostAsync(updateInput, byteContent).Result;
+
+        }
+        static async Task GetToDoListItemAsync(string token)
+        {
+            var client = new HttpClient();
+            Console.Write("What is the id of the to-do list item that you'd like to see? ");
+            var id = Console.ReadLine();
+            var url = $"https://one-list-api.herokuapp.com/items/{id}?access_token={token}";
+            var responseAsStream = await client.GetStreamAsync(url);
+            var item = await System.Text.Json.JsonSerializer.DeserializeAsync<Item>(responseAsStream);
+            Console.WriteLine();
+            var table = new ConsoleTable("Description", "Created At", "Updated At", "Completed");
+            table.AddRow(item.Text, item.CreatedAt, item.UpdatedAt, item.CompletedStatus);
+            Console.WriteLine(table);
+        }
+        static async Task DeleteToDoListItemAsync(string token)
+        {
+            var client = new HttpClient();
+            Console.Write("What is the id of the to-do list item that you'd like to delete? ");
+            var id = Console.ReadLine();
+            var url = $"https://one-list-api.herokuapp.com/items/{id}?access_token={token}";
+            await client.DeleteAsync(url);
+        }
+        static async Task UpdateToDoListItemCompleteAsync(string token)
+        {
+            var client = new HttpClient();
+
+            Console.Write("What is the id of the to-do list item that you'd like to mark complete? ");
+            var id = Console.ReadLine();
+            var updateInput = "{\"complete\": \"true\"}";
+            var updateContent = new StringContent(updateInput, Encoding.UTF8, "application/json");
+            var url = $"https://one-list-api.herokuapp.com/items/{id}?access_token={token}";
+            var responseAsStream = await client.PutAsync(url, updateContent);
+        }
+        static async Task UpdateToDoListItemIncompleteAsync(string token)
+        {
+            var client = new HttpClient();
+
+            Console.Write("What is the id of the to-do list item that you'd like to mark incomplete? ");
+            var id = Console.ReadLine();
+            var updateInput = "{\"complete\": \"false\"}";
+            var updateContent = new StringContent(updateInput, Encoding.UTF8, "application/json");
+            var url = $"https://one-list-api.herokuapp.com/items/{id}?access_token={token}";
+            var responseAsStream = await client.PutAsync(url, updateContent);
+        }
+        static string Menu()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("(S) See to-do list");
+            Console.WriteLine("(A) Add to-do list item");
+            Console.WriteLine("(G) Get to-do list item (must know item id)");
+            Console.WriteLine("(MC) Mark to-do list item complete (must know item id)");
+            Console.WriteLine("(MI) Mark to-do list item incomplete (must know item id)");
+            Console.WriteLine("(Q) Quit the application");
+            Console.WriteLine();
+            Console.Write("Select an option and press Enter: ");
+            var choice = Console.ReadLine().ToUpper();
+            return choice.ToUpper();
+        }
+        static async Task Main(string[] args)
+        {
+
+            Console.Clear();
             var token = "";
             if (args.Length == 0)
             {
-                Console.Write("What list would you like? ");
-                token = Console.ReadLine();
+                Console.Write("What list would you like? (Press Enter for Preset List) ");
+                var choice = Console.ReadLine();
+                if (choice == "")
+                {
+                    token = "brady_grapentine";
+                }
+                else
+                {
+                    token = choice;
+                }
             }
             else
             {
                 token = args[0];
             }
-            var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
-            //try:
-            var responseAsStream = await client.GetStreamAsync(url);
-            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
-            var table = new ConsoleTable("Description", "Created At", "Completed");
-            foreach (var item in items)
+            var counter = 0;
+            while (token != null)
             {
-                table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
+                var menuSelection = Menu();
+                switch (menuSelection)
+                {
+                    case "S":
+                        Console.Clear();
+                        Console.WriteLine();
+                        await SeeAllToDoListItemsAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "A":
+                        Console.Clear();
+                        await CreateToDoListItemAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "G":
+                        Console.Clear();
+                        await GetToDoListItemAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "D":
+                        Console.Clear();
+                        await DeleteToDoListItemAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "MC":
+                        Console.Clear();
+                        await UpdateToDoListItemCompleteAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "MI":
+                        Console.Clear();
+                        await UpdateToDoListItemIncompleteAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "Q":
+                        Console.Clear();
+                        Console.WriteLine("Closing application...");
+                        token = null;
+                        break;
+                    default:
+                        if (counter > 3)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Closing application...");
+                            token = null;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            counter += 1;
+                            Console.WriteLine();
+                            Console.WriteLine("Try again");
+                            Console.WriteLine();
+                            break;
+                        }
+                        break;
+                }
             }
-            table.Write();
+            Console.WriteLine();
+            Console.WriteLine("Goodbye");
+            Console.WriteLine();
         }
     }
 }
